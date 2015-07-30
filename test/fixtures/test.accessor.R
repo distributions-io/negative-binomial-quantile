4ddeb910b3ec9ecf1af49e575f44485df5525ae6
Planeshifter <pburckhardt@outlook.com>
2015-07-30 22:18:45 +0200

    [INIT]

---
+++ test/fixtures/test.accessor.R
@@ -0,0 +1,19 @@
+options( digits = 16 )
+library( jsonlite )
+
+
+r = 1
+p = 0.5
+probs = seq( 0, 1, 0.01 )
+y = qnbinom( probs, r, p )
+
+cat( y, sep = ",\n" )
+
+data = list(
+	r = r,
+	p = p,
+	data = probs,
+	expected = y
+)
+
+write( toJSON( data, digits = 16, auto_unbox = TRUE ), "./test/fixtures/accessor.json" )
